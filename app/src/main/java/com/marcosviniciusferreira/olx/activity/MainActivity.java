7fc92fc8fb86cd7ac7cd1906e859c6a362b40043
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-22 18:28:06 -0300

    firebase creating user and sign in methods

--- app/src/main/java/com/marcosviniciusferreira/olx/activity/MainActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/olx/activity/MainActivity.java
@@ -1,14 +1,22 @@
 package com.marcosviniciusferreira.olx.activity;
 
+import android.content.pm.ConfigurationInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.marcosviniciusferreira.olx.R;
+import com.marcosviniciusferreira.olx.helper.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +24,16 @@ public class MainActivity extends AppCompatActivity {
     private Switch tipoAcesso;
     private Button botaoAcessar;
 
+    private FirebaseAuth autenticacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         inicializarComponentes();
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
 
         botaoAcessar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,7 +48,37 @@ public class MainActivity extends AppCompatActivity {
                         //Verificar o estado do switch
                         if (tipoAcesso.isChecked()) {//Cadastro
 
+                            autenticacao.createUserWithEmailAndPassword(
+                                    email, senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Cadastro realizado com sucesso!", Toast.LENGTH_LONG).show();
+
+                                    } else {
+
+                                        Toast.makeText(MainActivity.this, "Erro ao cadastrar!", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
                         } else {//Login
+
+                            autenticacao.signInWithEmailAndPassword(
+                                    email, senha
+                            ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Login realizado com sucesso!", Toast.LENGTH_LONG).show();
+
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Erro ao logar", Toast.LENGTH_LONG).show();
+
+                                    }
+                                }
+                            });
 
                         }
 
