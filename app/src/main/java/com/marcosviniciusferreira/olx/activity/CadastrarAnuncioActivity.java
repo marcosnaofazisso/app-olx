505bddaa6bc8da7edc43e1e6be20541df66d30ab
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-22 19:52:20 -0300

    add my ads activity along with registrer new ad design

---
+++ app/src/main/java/com/marcosviniciusferreira/olx/activity/CadastrarAnuncioActivity.java
@@ -0,0 +1,15 @@
+package com.marcosviniciusferreira.olx.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marcosviniciusferreira.olx.R;
+
+public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_anuncio);
+    }
+}
---
+++ app/src/main/java/com/marcosviniciusferreira/olx/activity/MeusAnunciosActivity.java
@@ -0,0 +1,32 @@
+package com.marcosviniciusferreira.olx.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.marcosviniciusferreira.olx.R;
+
+public class MeusAnunciosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_anuncios);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CadastrarAnuncioActivity.class));
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
