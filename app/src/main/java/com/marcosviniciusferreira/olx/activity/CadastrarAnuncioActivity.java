7668740267c8eb093cc5be2397d2c31e3692f018
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-22 20:18:07 -0300

    add currency library and apllied it on register ad activity

--- app/src/main/java/com/marcosviniciusferreira/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/olx/activity/CadastrarAnuncioActivity.java
@@ -2,14 +2,39 @@ package com.marcosviniciusferreira.olx.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.marcosviniciusferreira.olx.R;
 
+import java.util.Locale;
+
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
+    private Button buttonCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        inicializarComponentes();
+
+
     }
+
+    private void inicializarComponentes() {
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoValor = findViewById(R.id.editValor);
+        campoDescricao = findViewById(R.id.editDescricao);
+        buttonCadastrar = findViewById(R.id.buttonCadastrarAnuncio);
+
+        //Configurar localidade para pt-BR
+        Locale locale = new Locale("pt", "BR");
+        campoValor.setLocale(locale);
+    }
+
 }
