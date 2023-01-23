00d2211951cc751de7ee561017ffd4effa0c58a7
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-23 16:27:05 -0300

    add ad images to be registered

--- app/src/main/java/com/marcosviniciusferreira/olx/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/olx/activity/CadastrarAnuncioActivity.java
@@ -1,34 +1,48 @@
 package com.marcosviniciusferreira.olx.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.marcosviniciusferreira.olx.R;
 import com.marcosviniciusferreira.olx.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity
+        implements View.OnClickListener {
 
     private EditText campoTitulo, campoDescricao;
     private CurrencyEditText campoValor;
     private Button buttonCadastrar;
     private MaskEditText campoTelefone;
+    private ImageView imagem1, imagem2, imagem3;
+
 
     private String[] permissoes = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
+
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +63,14 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         campoDescricao = findViewById(R.id.editDescricao);
         campoTelefone = findViewById(R.id.editTelefone);
         buttonCadastrar = findViewById(R.id.buttonCadastrarAnuncio);
+        
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+
+        imagem1.setOnClickListener(this);
+        imagem2.setOnClickListener(this);
+        imagem3.setOnClickListener(this);
 
         //Configurar localidade para pt-BR
         Locale locale = new Locale("pt", "BR");
@@ -85,4 +107,51 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.imageCadastro1:
+                escolherImagem(1);
+                break;
+            case R.id.imageCadastro2:
+                escolherImagem(2);
+                break;
+            case R.id.imageCadastro3:
+                escolherImagem(3);
+                break;
+        }
+    }
+
+    public void escolherImagem(int requestCode) {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            //Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+            //Configurar imagem no imageview
+            if (requestCode == 1) {
+                imagem1.setImageURI(imagemSelecionada);
+            } else if (requestCode == 2) {
+                imagem2.setImageURI(imagemSelecionada);
+
+            } else if (requestCode == 3) {
+                imagem3.setImageURI(imagemSelecionada);
+
+            }
+            listaFotosRecuperadas.add(caminhoImagem);
+
+        }
+
+    }
 }
