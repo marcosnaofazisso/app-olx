3117d15ecbcfd151a77bdae8c025ace137b23c50
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-01-26 10:00:13 -0300

    add carousel to ad detail

--- app/src/main/java/com/marcosviniciusferreira/olx/activity/DetalhesProdutoActivity.java
+++ app/src/main/java/com/marcosviniciusferreira/olx/activity/DetalhesProdutoActivity.java
@@ -2,14 +2,62 @@ package com.marcosviniciusferreira.olx.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.marcosviniciusferreira.olx.R;
+import com.marcosviniciusferreira.olx.model.Anuncio;
+import com.squareup.picasso.Picasso;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class DetalhesProdutoActivity extends AppCompatActivity {
+
+    private CarouselView carouselView;
+    private TextView titulo, descricao, estado, preco;
+    private Anuncio anuncioSelecionado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_produto);
+
+        //Configurar toolbar
+
+        inicializarComponentes();
+
+        //Recuperar dados do anuncio para exibicao
+        anuncioSelecionado = (Anuncio) getIntent().getSerializableExtra("anuncioSelecionado");
+
+        if (anuncioSelecionado != null) {
+
+            titulo.setText(anuncioSelecionado.getTitulo());
+            descricao.setText(anuncioSelecionado.getDescricao());
+            estado.setText(anuncioSelecionado.getEstado());
+            preco.setText(anuncioSelecionado.getValor());
+
+            ImageListener imageListener = new ImageListener() {
+                @Override
+                public void setImageForPosition(int position, ImageView imageView) {
+                    String urlString = anuncioSelecionado.getFotos().get(position);
+                    Picasso.get().load(urlString).into(imageView);
+                }
+            };
+
+            carouselView.setPageCount(anuncioSelecionado.getFotos().size());
+            carouselView.setImageListener(imageListener);
+
+
+        }
+
+    }
+
+    private void inicializarComponentes() {
+        carouselView = findViewById(R.id.carouselView);
+        titulo = findViewById(R.id.textTituloDetalhe);
+        descricao = findViewById(R.id.textDescricaoDetalhe);
+        estado = findViewById(R.id.textEstadoDetalhe);
+        preco = findViewById(R.id.textPrecoDetalhe);
     }
 }
